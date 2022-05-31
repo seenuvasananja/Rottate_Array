6643c8e5e06d522a26a8e09161be898f9d3ea3ff
Seenuvasan <seenuvasan@jetzerp.com>
2022-05-31 11:32:21 +0530

    Signed-off-by: Seenuvasan <seenuvasan@jetzerp.com>

---
+++ Alternative Sorting/Alternate_Sorting/Alternate_Sorting/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alternate_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alternate Sorting");
+            int size;
+            Console.WriteLine("Enter the Size of an Array");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] InputArray = new int[size];
+            List<int> list = new List<int>();
+            Console.WriteLine("Enter the Element of an Array");
+            for(int i=0;i<size;i++)
+            {
+                InputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Before Alternate Sorting");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(InputArray[i]);
+            }
+            Console.WriteLine("After Alternate Sorting");
+            list = Alternate_Sorting(size, InputArray);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static List<int> Alternate_Sorting(int size,int[] InputArray)
+        {
+            int temp = 0;
+            List<int> output = new List<int>();
+            for (int i=0;i<= size-1;i++)
+            {
+                for (int j=i+1;j<size;j++)
+                {
+                    if (InputArray[i]> InputArray[j])
+                    {
+                        temp = InputArray[i];
+                        InputArray[i] = InputArray[j];
+                        InputArray[j] = temp;
+                    }
+
+                }
+            }
+
+           
+            int p = 0, q = size - 1;
+            while (p<q)
+            {
+                
+                output.Add(InputArray[q--]);
+                output.Add(InputArray[p++]);
+
+            }
+            if (size%2 != 0)
+            {
+                output.Add(InputArray[p]);
+            }
+            return output;
+        }
+    }
+}
