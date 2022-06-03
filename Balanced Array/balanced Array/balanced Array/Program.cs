3afeb45dbf6e4056daa2b45b892598008372c394
Seenuvasan <seenuvasan@jetzerp.com>
2022-06-03 11:33:15 +0530

    Signed-off-by: Seenuvasan <seenuvasan@jetzerp.com>

---
+++ Balanced Array/balanced Array/balanced Array/Program.cs
@@ -0,0 +1,63 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace balanced_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Balanced Array");
+            int size;
+            Console.WriteLine("Enter the size of an Array");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] InputArray = new int[size];
+            int OutputList;
+            Console.WriteLine("Enter the Array Element");
+            for (int i =0;i< size;i++)
+            {
+                InputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Before Shuffled");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(InputArray[i]);
+            }
+            Console.WriteLine("After Shuffled");
+            OutputList = Balanced_array(size, InputArray);
+            Console.WriteLine(OutputList);
+            Console.ReadLine();
+           
+        }
+        public static int Balanced_array(int size,int[] InpuArray)
+        {
+            int output;
+            int[] LeftSumArray = new int[size];
+            int[] RightSumArray = new int[size];
+            LeftSumArray[0] = InpuArray[0];
+            
+            for (int i=1;i< InpuArray.Length;i++)
+            {
+
+                LeftSumArray[i] = InpuArray[i] + LeftSumArray[i - 1] ;  
+            }
+            RightSumArray[size - 1] = InpuArray[size - 1];
+
+            for (int i = size-2;i>=0;i--)
+            {
+                RightSumArray[i] = InpuArray[i] + RightSumArray[i + 1];
+            }
+            for (int i=1;i< size-1;i++)
+            {
+                if (LeftSumArray[i]== RightSumArray[i])
+                {
+                    output = InpuArray[i];
+                    return output;
+                }
+            }
+
+            return -1;
+
+        }
+    }
+}
